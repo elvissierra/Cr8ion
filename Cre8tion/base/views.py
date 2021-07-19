6293524f214f7cd6db00e90b04441999fe336f7a
elvis <elvissierra3@gmail.com>
2021-07-18 23:40:54 -0500

    utd

---
+++ Cre8tion/base/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def threedModels(request):
+    return HttpResponse('3dmodels')
+# Create your views here.
--- Cre8tion/creation/settings.py
+++ Cre8tion/creation/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cre8tion.apps.Cre8tionConfig',
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
