92f2eeb31c73dc908b923a739a5fb669420b1d44
elvis <elvissierra3@gmail.com>
2021-07-28 16:30:27 -0500

    utd

--- Cre8tion/base/models.py
+++ Cre8tion/base/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Prints(models.Model):
     title = models.CharField(max_length=30)
     creator = models.CharField(max_length=30)
--- Cre8tion/base/urls.py
+++ Cre8tion/base/urls.py
@@ -1,11 +1,10 @@
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView 
 from django.urls import path
-from .views import CustomLoginView, PrintsDetail, RegisterPage
-from django.contrib.auth.views import LogoutView
+from .views import PrintsDetail
 
 urlpatterns = [
-   path('login/', CustomLoginView.as_view(), name= 'login'),
-   path('logout/', LogoutView.as_view(next_page= 'login'), name= 'logout'),
-   path('register/', RegisterPage.as_view(), name= 'register'),
-   path('stl/', PrintsDetail.as_view(), name= 'stl'),
+   #path('login/', CustomLoginView.as_view(), name= 'login'),
+   path('stls', PrintsDetail.as_view(), name= 'prints'),
+   #path('logout/', LogoutView.as_view(next_page= 'login'), name= 'logout'),
+   #path('register/', RegisterPage.as_view(), name= 'register'),
 ]
