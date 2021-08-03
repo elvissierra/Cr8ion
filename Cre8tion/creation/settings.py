a2b7fa145639c84e832deed04d7ef8513fc1ddf7
elvis <elvissierra3@gmail.com>
2021-08-03 08:29:53 -0500

    utd

--- Cre8tion/creation/settings.py
+++ Cre8tion/creation/settings.py
@@ -46,7 +46,7 @@ ROOT_URLCONF = 'creation.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
