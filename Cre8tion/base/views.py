f840edbb57d152251d27c8cb981926e4b3d4314a
elvis <elvissierra3@gmail.com>
2021-08-29 22:37:57 -0500

    notes

--- Cre8tion/base/views.py
+++ Cre8tion/base/views.py
@@ -3,27 +3,29 @@ from django.contrib.auth.models import User, auth
 from .models import Prints
 
 
-    
+# identifying how to upload stl files and have every file viewable by every user.
 def index(request):
-    return render(request, 'index.html')
-def index1(request):
-    if request.method=='POST':
-        filename= request.POST['filename']
-        
-        upload1= request.FILES['upload']
-        object= Prints.objects.create(filename=filename,upload= upload1)
-        object.save()
-    context= Prints.objects.all()
-    return render(request, 'index1.html', {'context':context})
+    return render(request, "index.html")
 
-#class CustomLoginView(LoginView):
+
+def index1(request):
+    if request.method == "POST":
+        filename = request.POST["filename"]
+
+        upload1 = request.FILES["upload"]
+        object = Prints.objects.create(filename=filename, upload=upload1)
+        object.save()
+    context = Prints.objects.all()
+    return render(request, "index1.html", {"context": context})
+
+
+# class CustomLoginView(LoginView):
 #    template_name = 'base/login.html'
 #    fields = '__all__'
 #   redirect_authenticated_user = True
 
-#class RegisterPage(FormView):
+# class RegisterPage(FormView):
 #    template_name= 'base/register.html'
 #    form_class = UserCreationForm
 #    redirect_authentication_user = True
 #    success_url = reverse_lazy ('prints')
-
