350e8653b68fd651999038d563d9023325e8222b
elvis <elvissierra3@gmail.com>
2021-09-01 22:26:17 -0500

    utd form

--- Cre8tion/base/models.py
+++ Cre8tion/base/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.db.models.fields import BLANK_CHOICE_DASH
+
+# from django.contrib.auth.models import User
 
 # user = models.ForeignKey(
 # User, on_delete=models.CASCADE, null=True, blank=True)
@@ -11,7 +13,7 @@ class Prints(models.Model):
     description = models.TextField(max_length=100, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     stl = models.FileField(upload_to="stls")
-    cover = models.ImageField(upload_to="covers")
+    cover = models.ImageField(upload_to="covers", null=True, blank=True)
 
     def __str__(self):
         return self.filename
