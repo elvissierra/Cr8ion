1d151a4a6af0831034e53c08375763af142c3443
elvis <elvissierra3@gmail.com>
2021-09-21 11:58:11 -0500

    js

---
+++ Cre8tion/base/template/base/main.js
@@ -0,0 +1,21 @@
+
+
+sidenav.onscroll = function () { stkyfunc() };
+
+var snheader = document.getElementById("snheader");
+
+var sticky = snheader.offsetTop;
+
+function stkyfunc() {
+    if (sidenav.pageYOffset >= sticky) {
+        snheader.classList.add("sticky")
+    } else {
+        snheader.classList.remove("sticky");
+    }
+}
+
+
+function popupfunc() {
+    var btn = document.getElementById("popup");
+    btn.classList.toggle("show");
+}
