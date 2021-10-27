4101f205ea8f3ce43d9d465ec2c76e654fc7a292
elvis <elvissierra3@gmail.com>
2021-10-26 22:54:07 -0500

    comment model

--- Cre8tion/base/models.py
+++ Cre8tion/base/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import tree
+
+# from django.contrib.auth.models import User
 
 
 class Print(models.Model):
@@ -9,8 +12,17 @@ class Print(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     stl = models.FileField(upload_to="store/stls/")
     cover = models.ImageField(upload_to="store/covers/", null=True, blank=True)
-    likes = models.ManyToManyField(User, related_name="likes")
-    dislikes = models.ManyToManyField(User, related_names="dislikes")
+    likes = models.ManyToManyField(User, blank=True, related_name="like")
+    dislikes = models.ManyToManyField(User, blank=True, related_name="dislike")
+    like_count = models.BigIntegerField(default="0")
 
     def __str__(self):
         return self.title
+
+
+# class Post(models.Model):
+#    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+#    comment = models.TextField()
+#    likes = models.ManyToManyField(User, blank=True, null=True, related_name="like")
+#    dislikes = models.ManyToManyField(User, blank= True, null= True, related_name= "dislike")
+#    like_count = models.BigIntegerField(default="0")
