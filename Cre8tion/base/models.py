3a3a9276af0fd668de86c1b6cd258433d752fc60
elvis <elvissierra3@gmail.com>
2021-10-27 23:56:13 -0500

    utd

--- Cre8tion/base/models.py
+++ Cre8tion/base/models.py
@@ -10,6 +10,7 @@ class Print(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField(null=True, blank=True)
     stl = models.FileField(upload_to="store/stls/")
     cover = models.ImageField(upload_to="store/covers/", null=True, blank=True)
     likes = models.ManyToManyField(User, blank=True, related_name="like")
@@ -24,5 +25,7 @@ class Print(models.Model):
 #    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 #    comment = models.TextField()
 #    likes = models.ManyToManyField(User, blank=True, null=True, related_name="like")
-#    dislikes = models.ManyToManyField(User, blank= True, null= True, related_name= "dislike")
-#    like_count = models.BigIntegerField(default="0")
+#    dislikes = models.ManyToManyField(
+#        User, blank=True, null=True, related_name="dislike"
+#    )
+#    like_count = models.IntegerField(default="0")
