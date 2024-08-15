8c53de55c6d1a7686d8bb0c218eb2e76b91bf74a
elvissierra <elvissierra3@gmail.com>
2024-08-15 16:28:57 -0500

    path

--- Cr8ion/urls.py
+++ Cr8ion/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 app_name = "cr8"
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("cr8.urls")),
+    path("models/", include("cr8.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
