c8932ffa6c33eea4fb22109a9494bd6093de64fa
elvissierra <elvissierra3@gmail.com>
2024-11-20 16:04:15 -0600

    clean instances

--- cr8_api/apps/prints/models.py
+++ cr8_api/apps/prints/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
-from django.contrib.contenttypes.models import ContentType
+
+def default_user():
+    user, created = User.objects.get_or_create(
+        username = "default user",
+        defaults = {"email" : "default@gmail.com"}
+    )
+    return user
 
 class Print(models.Model):
-    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, default=default_user)
     title = models.CharField(max_length=50)
-    description = models.TextField(null=True, blank=True)
+    description = models.TextField(max_length=50, default="empty")
     created = models.DateTimeField(auto_now_add=True)
     stl = models.FileField(upload_to="store/stls/")
     cover = models.ImageField(upload_to="store/covers/", null=True, blank=True)
---
+++ scripts/populate.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import django
+import uuid
+import random
+import string
+from django.core.files import File
+
+# Set up the Django environment
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cr8_api.settings')
+django.setup()
+
+from cr8_api.apps.prints.models import Print
+from django.contrib.auth.models import User
+
+def populate_db():
+    covers_root = "store/covers/"
+    stls_root = "store/stls/"
+
+    c_files = os.listdir(covers_root)
+    s_files = os.listdir(stls_root)
+
+    # Get or create a default user
+    default_user, _ = User.objects.get_or_create(username='default_user', defaults={'email': 'default@example.com'})
+
+    for c, s in zip(c_files, s_files):
+        generated_uuid = uuid.uuid4()
+        dummy_str = ''.join(random.choices(string.ascii_letters, k=7))
+        cover_path = os.path.join(covers_root, c)
+        stl_path = os.path.join(stls_root, s)
+
+        with open(cover_path, "rb") as cover_obj, open(stl_path, "rb") as stl_obj:
+            cover = File(cover_obj)
+            stl = File(stl_obj)
+
+            # Create the Print object
+            Print.objects.create(
+                id=generated_uuid,
+                creator=default_user,
+                title=dummy_str,
+                description=dummy_str,
+                cover=cover,
+                stl=stl,
+            )
+
+if __name__ == "__main__":
+    populate_db()
